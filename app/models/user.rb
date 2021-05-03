af7c66e905dbe44db1f7d621fe113fde648dd0dd
danilol <dcanadablandon@gmail.com>
2021-05-03 10:53:51 -0600

    add friendship model, migratio, and the self-referential relacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def stock_alredy_tracked?(ticker_symbol)
     return false unless !stocks.empty?
